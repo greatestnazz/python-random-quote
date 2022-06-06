5f062250ee960857c0674584d403a86e22d78cb6
nasir <mnasshuaib@gmail.com>
2022-06-06 17:48:12 +0100

    random choice from the array

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
+
 def primary():
   #print("Keep it logically awesome.")
-
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-
-  print(quotes[13])
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
+    
+    quo = random.choice(quotes)
+    print(quo)
 
 if __name__== "__main__":
-  primary()
+    primary()
