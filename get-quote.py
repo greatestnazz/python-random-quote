72a287ed2435ea4d6597daeedd648d3752693a56
nasir <mnasshuaib@gmail.com>
2022-06-07 10:13:47 +0100

    Changes to either add or read qoute

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,31 @@
 import random
+import sys
 
+act = input("What would you like to do, add or read a quote? \n")
+
+
+#Defining Function for read quote
 def primary():
   #print("Keep it logically awesome.")
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
     
-    quo = random.choice(quotes)
-    print(quo)
+    for i in range(0,2):
+        quo = random.choice(quotes)
+        print(quo)
+#Defining Function for add quote
+def secondary():
+    quot = input("Please enter your quote? \n\n")    
+    sys.stdout = open('quotes.txt', 'a')
+    print(quot)    
 
-if __name__== "__main__":
+#code to get which function to run
+if act == "read":
     primary()
+else:
+    secondary()    
+
+    
+#if __name__== "__main__":
+    #primary()
